600c58bc10fa34ddf16f37195677cae63cbd1f18
Priyy08 <123939050+Priyy08@users.noreply.github.com>
2025-07-01 11:14:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,9 +54,8 @@ import ResultsDisplay from './components/ResultsDisplay.jsx';
 import { FaRobot } from "react-icons/fa";
 import './App.css';
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
-const API_BASE_URL = "https://priyanshu-madi-wasserstoff-aiinterntask.onrender.com";
 
 function App() {
   const [results, setResults] = useState(null);
